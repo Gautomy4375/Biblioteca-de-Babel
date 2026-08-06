1dfdfa7f365828db74163c6be91aab03685ab216
gautomy4375 <gaudiotomy@gmail.com>
2026-08-06 10:49:35 -0300

    Finished with sql schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
     UserID INT PRIMARY KEY,
     Email VARCHAR(64) NOT NULL UNIQUE, 
-    Password VARCHAR(64) NOT NULL, 
-    Username VARCHAR(64),
+    Password VARCHAR(256) NOT NULL, 
+    Username VARCHAR(256) UNIQUE, 
     Region CHAR(3),
     Avatar TEXT,
     Fecha_Registro DATE,
     Notifications BOOLEAN,
     Perfil_Publico BOOLEAN,
-    GoogleID VARCHAR(30),
+    GoogleID VARCHAR(30) UNIQUE
 )
-CREATE TABLE books IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS books (
     BookID INT PRIMARY KEY,
     Title VARCHAR(512),
     Sinopsis TEXT, 
@@ -21,92 +21,102 @@ CREATE TABLE books IF NOT EXISTS (
     Link_Compra TEXT,
     Link_Gratis TEXT,
     Fecha_Publicacion DATE,
-    Google_Books_ID VARCHAR(512) NOT NULL UNIQUE, 
+    Google_Books_ID VARCHAR(30) NOT NULL UNIQUE, 
     Fecha_Cacheada DATE,
-    Cantidad_Interacciones SMALLINT,
+    Cantidad_Interacciones SMALLINT
 )
-CREATE TABLE categorias IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS categorias (
     CategoryID INT PRIMARY KEY,
-    Category_Name VARCHAR(64) NOT NULL,
+    Category_Name VARCHAR(64) NOT NULL
 )
-CREATE TABLE book_in_category IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS book_in_category (
     BookID INT,
     CategoryID INT,
 
-    PRIMARY KEY(BookID, CategoryID)
-    
-    FOREIGN KEY (BookID) REFERENCES books(BookID)
+    PRIMARY KEY(BookID, CategoryID),
+
+    FOREIGN KEY (BookID) REFERENCES books(BookID),
     FOREIGN KEY (CategoryID) REFERENCES categorias(CategoryID)
 )
-CREATE TABLE preference_category IF NOT EXISTS (
-    UserID INT PRIMARY KEY,
-    CategoryID INT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS preference_category (
+    UserID INT,
+    CategoryID INT, 
     Preference_Weight SMALLINT,  
+
+    PRIMARY KEY(UserID, CategoryID),
 
      FOREIGN KEY (UserID) REFERENCES users(UserID),
      FOREIGN KEY (CategoryID) REFERENCES categorias(CategoryID)
 )
-CREATE TABLE preferences IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS preferences (
     PreferenceID INT PRIMARY KEY,
-    UserID INT PRIMARY KEY,
-    BookID INT PRIMARY KEY,
+    UserID INT NOT NULL,
+    BookID INT NOT NULL,
 
+     UNIQUE(BookID, UserID),
      FOREIGN KEY (UserID) REFERENCES users(UserID),
      FOREIGN KEY (BookID) REFERENCES books(BookID)
 )
-CREATE TABLE books_progress IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS books_progress (
     ProgressID INT PRIMARY KEY,
-    UserID INT PRIMARY KEY, 
-    BookID INT PRIMARY KEY, 
+    UserID INT NOT NULL, 
+    BookID INT NOT NULL, 
     Book_State VARCHAR(13), -- 13 porque el más grande es "abandonado". Lo voy a cambiar dsp si cambiamos como funciona esto.
     /* Se puede cambiar a ENUM si confirmamos que vamos a usar un POSGRESQL */
     Puntuacion INT, 
     Amount_Read INT, 
     Starting_Date DATE, 
 
+     /* UNIQUE(BookID, UserID), */  -- Se puede agregar despues. no estoy seguro como va a funcionar esto asi q por ahora lo dejo asi
      FOREIGN KEY (UserID) REFERENCES users(UserID),
      FOREIGN KEY (BookID) REFERENCES books(BookID)
 )
-CREATE TABLE reviews IF NOT EXISTS (
-    BookID INT PRIMARY KEY,
-    UserID INT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS reviews (
+    BookID INT NOT NULL,
+    UserID INT NOT NULL, 
     ReviewID INT PRIMARY KEY, 
     Review_Content TEXT, 
     Creation_Date DATE,
 
+    UNIQUE(BookID, UserID),
      FOREIGN KEY (UserID) REFERENCES users(UserID),
      FOREIGN KEY (BookID) REFERENCES books(BookID)
 )
-CREATE TABLE followers IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS followers (
     FollowingID INT PRIMARY KEY,
-    FollowerID INT PRIMARY KEY, 
-    FollowedID INT PRIMARY KEY,
+    FollowerID INT NOT NULL, 
+    FollowedID INT NOT NULL,
     Following_Date DATE, 
 
+    UNIQUE(FollowerID, FollowedID),
     FOREIGN KEY (FollowerID) REFERENCES users(UserID),
-
     FOREIGN KEY (FollowedID) REFERENCES users(UserID)
 )
-CREATE TABLE list IF NOT EXISTS (
-    ListID INT PRIMARY KEY,
-    UserID INT PRIMARY KEY, 
-    List_Name VARCHAR(64) PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS lists (
+    listsID INT PRIMARY KEY,
+    UserID INT NOT NULL, 
+    lists_Name VARCHAR(64) NOT NULL, 
     Is_Public BOOLEAN,
     Date_Created DATE, 
 
-     FOREIGN KEY (UserID) REFERENCES users(UserID),
+    UNIQUE(UserID, lists_Name),
+     FOREIGN KEY (UserID) REFERENCES users(UserID)
 )
-CREATE TABLE books_in_list IF NOT EXISTS (
-    Book_In_ListID INT PRIMARY KEY,
-    BookID INT PRIMARY KEY,
-    ListID INT PRIMARY KEY, 
-    Order SMALLINT, 
+CREATE TABLE IF NOT EXISTS books_in_lists (
+    Book_In_listsID INT PRIMARY KEY,
+    BookID INT NOT NULL,
+    listsID INT NOT NULL, 
+    Book_Order SMALLINT, 
 
-    FOREIGN KEY (BookID) REFERENCES books(BookID)
+    UNIQUE(BookID, listsID),
+    FOREIGN KEY (BookID) REFERENCES books(BookID),
+    FOREIGN KEY (listsID) REFERENCES lists(listsID)
 )
-CREATE TABLE likes IF NOT EXISTS (
-    UserID INT PRIMARY KEY,
-    ListID INT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS likes (
+    UserID INT,
+    listsID INT, 
 
+    PRIMARY KEY(UserID, listsID),
      FOREIGN KEY (UserID) REFERENCES users(UserID), 
+     FOREIGN KEY (listsID) REFERENCES lists(listsID)
 )
