5152231f097d4243cbf631125c044d5ffec4c049
gautomy4375 <gaudiotomy@gmail.com>
2026-08-06 09:45:47 -0300

    Create books table, category table, book in category table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -4,9 +4,32 @@ CREATE TABLE users IF NOT EXISTS (
     Password VARCHAR(32), 
     Username VARCHAR(64),
     Region CHAR(3),
-    Avatar VARCHAR(2048),
+    Avatar TEXT,
     Fecha_Registro DATE
     Notifications BOOLEAN
     Perfil_Publico BOOLEAN
     GoogleID VARCHAR(30)
+)
+CREATE TABLE books IF NOT EXISTS (
+    BookID VARCHAR(64) PRIMARY KEY,
+    Title VARCHAR(512) PRIMARY KEY,
+    Sinopsis TEXT, 
+    Autores TEXT,
+    Portada TEXT,
+    Paginas INT,
+    Idioma CHAR(15),
+    Link_Compra TEXT,
+    Link_Gratis TEXT,
+    Fecha_Publicacion DATE,
+    Google_Books_ID VARCHAR(512) PRIMARY KEY, 
+    Fecha_Cacheada DATE,
+    Cantidad_Interacciones SMALLINT,
+)
+CREATE TABLE categorias IF NOT EXISTS (
+    CategoryID INT PRIMARY KEY,
+    Category_Name VARCHAR(64),
+)
+CREATE TABLE book_in_category IF NOT EXISTS (
+    BookID INT PRIMARY KEY,
+    CategoryID INT PRIMARY KEY,
 )
