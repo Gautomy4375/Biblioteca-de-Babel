875a19b6a6973c2dbedfbe10cdf5f40fcb5334cc
gautomy4375 <gaudiotomy@gmail.com>
2026-08-06 10:07:11 -0300

    Fixed commas

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -5,10 +5,10 @@ CREATE TABLE users IF NOT EXISTS (
     Username VARCHAR(64),
     Region CHAR(3),
     Avatar TEXT,
-    Fecha_Registro DATE
-    Notifications BOOLEAN
-    Perfil_Publico BOOLEAN
-    GoogleID VARCHAR(30)
+    Fecha_Registro DATE,
+    Notifications BOOLEAN,
+    Perfil_Publico BOOLEAN,
+    GoogleID VARCHAR(30),
 )
 CREATE TABLE books IF NOT EXISTS (
     BookID VARCHAR(64) PRIMARY KEY,
@@ -47,7 +47,7 @@ CREATE TABLE books_progress IF NOT EXISTS (
     ProgressID INT PRIMARY KEY,
     UserID INT PRIMARY KEY, 
     BookID INT PRIMARY KEY, 
-    Book_State VARCHAR(13) -- 13 porque el más grande es "abandonado". Lo voy a cambiar dsp si cambiamos como funciona esto.
+    Book_State VARCHAR(13), -- 13 porque el más grande es "abandonado". Lo voy a cambiar dsp si cambiamos como funciona esto.
     /* Se puede cambiar a ENUM si confirmamos que vamos a usar un POSGRESQL */
     Puntuacion INT, 
     Amount_Read INT, 
