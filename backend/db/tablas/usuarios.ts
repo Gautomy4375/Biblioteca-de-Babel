77043b849e0e4b9895cca5fea58dadcbc599e848
Tomas <gaudiotomy@gmail.com>
2026-08-12 12:01:22 -0300

    has special character checks

--- backend/db/tablas/usuarios.ts
+++ backend/db/tablas/usuarios.ts
@@ -12,6 +12,10 @@ function detectBadWords (user: string): boolean {
 }
 return isBad
 }
+function hasSpecialChar(user:string) {
+    const regex = /[^a-zA-Z0-9]/; // ni idea como funciona esta ecuacion pero funciona 
+    return regex.test(user);
+  }  
 
 function storeUser(user: string) {
     if (detectBadWords(user) === true)
@@ -21,3 +25,6 @@ function storeUser(user: string) {
         // Guardar usuario en base de datos / json
     }
 }
+
+console.log(hasSpecialChar("HelloWorld123")); 
+console.log(hasSpecialChar("Hello@World!")); 
