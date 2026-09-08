624d303f42447a818c27cac87ed35762626c3a47
tgsantu <tomasgarciasantu@gmail.com>
2026-09-08 15:43:57 -0300

    hice la logica de los errores en el sign up y log in con los status y el "@"

--- front/home/home.js
+++ front/home/home.js
@@ -3,7 +3,6 @@ let usernamesusave = "";
 let mailsusave = "";
 let passwordsusave = "";
 
-// Fixed: Target the correct element IDs
 const usernamesu = document.getElementById("usernamesu");
 const mailsu = document.getElementById("mailsu"); 
 const passwordsu = document.getElementById("passwordsu"); 
@@ -16,14 +15,27 @@ subtnsend.addEventListener("click", () => {
     passwordsusave = passwordsu.value;
 
     if (!mailsusave.includes("@")) {
-        console.log("ERROR: el mail no tiene @");
+        mensajesu.innerText = "Este e-mail no es válido."
+        mensajesu.style.color = "red"
     } else if (usernamesusave.includes("@")) {
-        console.log("ERROR: el usuario tiene @"); // Fixed text to match username check
+        mensajesu.innerhtml = 'El nombre de usuario no puede tener "@".'
+        mensajesu.style.color = "red"
     } else {
         console.log("Saved:", usernamesusave);
         console.log("Saved:", mailsusave);
         console.log("Saved:", passwordsusave);
+        //aca se manda al back
+        let status = "201" //lo pongo a mano para probar, pero llega del back
+        if (status === "201") { //todo bien
+            mensajesu.innerHTML = "Creación de cuenta exitosa.<br>Bienvenido username!" //donde dice "username" va el username
+            mensajesu.style.color = "green"
+            //window.location.href = "link del dashboard de usuario";
+            } else if (status === "409") { //400 es la contraseña mal y 404 es el usuario no existe
+            mensajesu.innerText = "Este nombre de usuario o un usuario con este mail ya existe."
+            mensajesu.style.color = "red"
+            } 
     }
+
 });
 
 //signin
@@ -42,9 +54,21 @@ sibtnsend.addEventListener("click", () => {
     usernamesisave = usernamesi.value;
     passwordsisave = passwordsi.value;
 
-        console.log("Saved:", usernamesisave);
-        console.log("Saved:", passwordsisave);
-    }
+    console.log("Saved:", usernamesisave);
+    console.log("Saved:", passwordsisave);
+
+    //aca se manda al back
+    let status = "200" //lo pongo a mano para probar, pero llega del back
+    if (status === "200") { //todo bien
+        mensajesi.innerText = "Bienvenido " + "username" +" !" //donde dice "username" va el username
+        mensajesi.style.color = "green"
+        //window.location.href = "link del dashboard de usuario";
+     } else if (status === "400"||status === "404") { //400 es la contraseña mal y 404 es el usuario no existe
+        mensajesi.innerText = "El nombre de usuario, el mail y/o la contarseña son incorrectos."
+        mensajesi.style.color = "red"
+     } 
+    
+}
 );
 
 //VISUALES
