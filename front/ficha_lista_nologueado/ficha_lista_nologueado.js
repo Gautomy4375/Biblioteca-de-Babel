413fa9ae8c3da2ef85994bfc24aa39df54b08342
tgsantu <tomasgarciasantu@gmail.com>
2026-09-09 15:30:21 -0300

    termine lista_nologueado con: - grid que se llena con la cantidad de libros - like

--- front/ficha_lista_nologueado/ficha_lista_nologueado.js
+++ front/ficha_lista_nologueado/ficha_lista_nologueado.js
@@ -19,7 +19,7 @@ subtnsend.addEventListener("click", () => {
         mensajesu.innerText = "Este e-mail no es válido."
         mensajesu.style.color = "red"
     } else if (usernamesusave.includes("@")) {
-        mensajesu.innerhtml = 'El nombre de usuario no puede tener "@".'
+        mensajesu.innerHTML = 'El nombre de usuario no puede tener "@".'
         mensajesu.style.color = "red"
     } else {
         console.log("Saved:", usernamesusave);
@@ -99,4 +99,28 @@ if (event.target === signinOverlay) {
 }
 });
 
+//#endregion
+
+//#region libroslista
+const datos = [
+  { imagen: "https://images.cdn2.buscalibre.com/fit-in/360x360/d9/1f/d91fe3e3e534deec6995a67458a17126.jpg", texto: "Primera tarjeta" },
+  { imagen: "https://images.cdn2.buscalibre.com/fit-in/360x360/d9/1f/d91fe3e3e534deec6995a67458a17126.jpg", texto: "Segunda tarjeta" },
+  { imagen: "https://images.cdn2.buscalibre.com/fit-in/360x360/d9/1f/d91fe3e3e534deec6995a67458a17126.jpg", texto: "Tercera tarjeta" }
+];
+
+const librosGrid = document.getElementById('libros-grid');
+// Seleccionamos la plantilla
+const plantilla = document.getElementById('libro-template');
+
+datos.forEach((item) => {
+  // 1. Duplicamos/clonamos la estructura de la plantilla
+  const clon = plantilla.content.cloneNode(true);
+
+  // 2. Buscamos la img y el p DENTRO del clon y les asignamos los datos
+  clon.querySelector('img').src = item.imagen;
+  clon.querySelector('p').textContent = item.texto;
+
+  // 3. Insertamos el clon en el contenedor principal
+  librosGrid.appendChild(clon);
+});
 //#endregion
