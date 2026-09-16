266648e4495944fbd3523120992f162277a383ad
Santiago <you@example.com>
2026-09-16 14:29:43 -0300

    poste masha y el oso

--- front/pagina_perfil_propio/pagina_perfil.js
+++ front/pagina_perfil_propio/pagina_perfil.js
@@ -1,10 +1,32 @@
 let enter = document.getElementById('busca');
 let input = document.getElementById('buscador');
 
-
+/*ytsma, aca empiezo con lo de hacer los datos dinamicos*/
 let nombre = document.getElementById('nom');
-nombre.textContent = 'nombre';
+nombre.textContent = 'Nombre propio';
 
+let seguidores = document.getElementById('seguidores');
+seguidores.textContent = '56';
+
+let seguidos = document.getElementById('seguidos');
+seguidos.textContent = '98';
+
+let leidos = document.getElementById('leidos');
+leidos.textContent = '27';
+
+let listas = document.getElementById('listas');
+listas.textContent = '12';
+
+let imagen_1 = document.getElementById('libro_uno')
+imagen_1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPS1Es-Brcsfc0g7ZQovCNTvLISpbpLoOOUOgvzSSNspl0wds7TdJMw&s=10';
+let imagen_2 = document.getElementById('libro_dos')
+imagen_2.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPS1Es-Brcsfc0g7ZQovCNTvLISpbpLoOOUOgvzSSNspl0wds7TdJMw&s=10';
+let imagen_3 = document.getElementById('libro_tres')
+imagen_3.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPS1Es-Brcsfc0g7ZQovCNTvLISpbpLoOOUOgvzSSNspl0wds7TdJMw&s=10';
+let imagen_4 = document.getElementById('libro_cuatro')
+imagen_4.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPS1Es-Brcsfc0g7ZQovCNTvLISpbpLoOOUOgvzSSNspl0wds7TdJMw&s=10';
+let imagen_5 = document.getElementById('libro_cinco')
+imagen_5.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPS1Es-Brcsfc0g7ZQovCNTvLISpbpLoOOUOgvzSSNspl0wds7TdJMw&s=10';
 
 
 enter.addEventListener('submit', function(event){
