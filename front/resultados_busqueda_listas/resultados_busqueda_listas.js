5fb1ba322997533b2756ade5e1a2576cde60e885
Santiago <you@example.com>
2026-09-23 11:53:17 -0300

    sistema de buqueda de libros termiando

--- front/resultados_busqueda_listas/resultados_busqueda_listas.js
+++ front/resultados_busqueda_listas/resultados_busqueda_listas.js
@@ -8,5 +8,45 @@ if(titu){
 
     conTi.textContent= `'${titu}'`;
 
-    buscarListas
+    buscarListas(titu);
+}
+
+async function buscarlistas(titu){
+
+    let endpoint = '/api'; //GAUDIO DAME LOS ENDPOINTSS
+
+    let respuesta = await fetch(endpoint);
+
+    if (respuesta.ok){
+        let listas = await respuesta.json();
+
+        if (listas.length > 0){
+            mostrarListas(libros);
+        }
+        else{
+            conLi.textContent = 'No se encontraron listas'
+        }
+    }
+    else{
+        conLi.textContent = 'Hubo un error'
+    }
+}
+
+function mostrarListas(libros){
+    conLi.innerHTML = '';
+
+    let i = 0;
+
+    if (i < listas.length){
+        let divs = document.createElement('a');
+        divs.classList.add('boton_lista');
+        //acava el href de las listas
+
+
+        divs.innerHTML=`
+        <img src='${listas.portada}'><img>
+        <h3>${listas.title}<h3>
+        `
+        i++
+    } 
 }
